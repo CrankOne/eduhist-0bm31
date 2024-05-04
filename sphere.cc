e7d7e7d7317cb57662cec1403c15d25cab5757d3
Renat Dusaev <rdusaev@cern.ch>
2024-05-04 11:00:14 +0200

    changes in sphere testing fixture (removed old code)

--- sphere.cc
+++ sphere.cc
@@ -5,20 +5,15 @@
 #include "sphere-uniform.hh"
 
 int main(int argc, char * argv[]) {
-
-
+    SphereUniform sg(); // TODO
 
     for(int i = 0; i < 5; ++i) {
-        float phi       = M_PI - 2*M_PI*rand()/RAND_MAX
-            //, theta     = M_PI*rand()/RAND_MAX
-            , cosTheta   = 1 - 2.*rand()/RAND_MAX
-            , theta     = acos(cosTheta)
+        float phi, theta;
+        sg.draw(&theta, &phi);
+        float x = sin(theta)*cos(phi) 
+            , y = sin(theta)*sin(phi)
+            , z = cos(theta)
             ;
-
-        //float x = sin(theta)*cos(phi) 
-        //    , y = sin(theta)*sin(phi)
-        //    , z = cos(theta)
-        //    ;
         printf("%e %e %e\n", x, y, z);
     }
     return 0;
